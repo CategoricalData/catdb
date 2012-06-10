c85e8e8b6adf2391de75e8a89da0578428043a78
Jason Gross <jgross@mit.edu>
2012-06-10 01:56:05 -0400

    Define [saturate].
    It works again, now.  The proof should probably be sped up/better automated.
    There might be a simpler way to do it, given that we were using [induction]
    before and now we're not.  Replacing [forall_equiv__eq] with a simpler version
    might also do it.

--- CategorySchemaEquivalence.v
+++ CategorySchemaEquivalence.v
@@ -16,18 +16,33 @@ Section Schema_Category_Equivalence.
   Hint Extern 1 (@RelationsEquivalent _ _ _ (PathsEquivalent _) _ _ _ _) => apply PreCompose.
 
   Definition path2path' s d (p : path S s d) : path' (Edge S) s d := p.
-(*
+
+  Hint Rewrite concatenate_p_noedges concatenate_noedges_p concatenate_associative.
+
+  Ltac replace_noedges :=
+        repeat match goal with
+             | [ H : ?rel NoEdges ?x |- _ ] => repeat rewrite <- H in *; clear H
+           end; rewrite concatenate_p_noedges in * || rewrite concatenate_noedges_p in *.
+
+  (* TODO: Speed this up, automate this better. *)
   Definition saturate : Category.
     refine {| Object := S;
-      Morphism := forall s d, @EquivalenceClassOf (path S s d) (@PathsEquivalent S s d);
+      Morphism := fun s d => EquivalenceClass (@PathsEquivalent S s d) (Reflexive _ s d) (Symmetric _ s d) (Transitive _ s d);
       (* foo := 1; *) (* uncommenting this line gives "Anomaly: uncaught exception Not_found. Please report."  Maybe I should report this?  But I haven't figured out a minimal test case. *)
-      Identity := (fun _ => NoEdges);
-      Compose := (fun _ _ _ m1 m2 => concatenate m2 m1)
-      |}; abstract (intros; solve [ t | match goal with
+      Identity := (fun _ => classOf NoEdges);
+      Compose := (fun s d d' m1 m2 => apply2_to_class (@concatenate S S.(Edge) s d d') (@concatenate_mor S s d d') m2 m1)
+    |}; intros; apply forall_equiv__eq;
+    repeat (simpl; intros; destruct_type or; destruct_hypotheses; repeat split);
+      repeat (replace_noedges; replace_inClass; intros); simpl;
+        repeat (replace_inClass; eexists; eauto); clear_inClass; try assumption || reflexivity; try replace_noedges;
+          repeat match goal with
+                   | [ H : context[PathsEquivalent] |- _ ] => rewrite H in *; clear H
+                 end; repeat rewrite concatenate_associative; try reflexivity.
+(* abstract (intros; solve [ t | match goal with
                                           | [ p : path _ _ _ |- _ ] => solve [ induction p; t ]
-                                        end ]).
+                                        end ]).*)
   Defined.
-*)
+
   Fixpoint compose_morphism_path s d (p : path' C.(Morphism) s d) : Morphism _ s d :=
     match p with
       | NoEdges => Identity s
