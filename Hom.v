5029e14f4f6cf8410e5c7e73546c27939a514ca3
Jason Gross <jgross@mit.edu>
2012-06-14 19:58:39 -0400

    Define Hom-functors
    Seeing how Adam dealt with opposite categories helped a lot, I think.
    
    I should probably add documentation...

---
+++ Hom.v
@@ -0,0 +1,52 @@
+Require Import Program.
+Require Export Category Functor.
+Require Import Common Duals ProductCategory SetCategory.
+
+Set Implicit Arguments.
+
+Local Infix "*" := ProductCategory.
+
+Section HomFunctor.
+  Variable C : Category.
+  Let COp := OppositeCategory C.
+
+  Section Covariant.
+    Variable A : COp.
+
+    Definition CovariantHomFunctor : Functor C TypeCat.
+      refine {| ObjectOf := (fun X : C => Morphism C A X : TypeCat);
+        MorphismOf := (fun X Y f => (fun g : Morphism C A X => Compose f g))
+        |}; abstract (simpl; intros; repeat (apply functional_extensionality_dep; intro); t_with t').
+    Defined.
+  End Covariant.
+
+  Section Contravariant.
+    Variable B : C.
+
+    Definition ContravariantHomFunctor : Functor COp TypeCat.
+      refine {| ObjectOf := (fun X : COp => Morphism COp B X : TypeCat);
+        MorphismOf := (fun X Y h => (fun g : Morphism COp B X => Compose h g))
+        |}; abstract (simpl; intros; repeat (apply functional_extensionality_dep; intro); t_with t').
+    Defined.
+  End Contravariant.
+
+  Definition hom_functor_object_of (c'c : Object (COp * C)) := Morphism C (fst c'c) (snd c'c) : TypeCat.
+
+  Definition hom_functor_morphism_of (s's : (COp * C)%type) (d'd : (COp * C)%type) (hf : Morphism (COp * C) s's d'd) : Morphism _ (hom_functor_object_of s's) (hom_functor_object_of d'd).
+    destruct s's as [ s' s ], d'd as [ d' d ].
+    destruct hf as [ h f ].
+    intro g.
+    exact (Compose f (Compose g h)).
+  Defined.
+
+  Definition HomFunctor : Functor (COp * C) TypeCat.
+    refine {| ObjectOf := (fun c'c : Object (COp * C) => Morphism C (fst c'c) (snd c'c) : TypeCat);
+      MorphismOf := (fun X Y (hf : Morphism (COp * C) X Y) => hom_functor_morphism_of X Y hf)
+      |};
+    abstract (
+      unfold hom_functor_morphism_of, hom_functor_object_of in *; simpl in *;
+        unfold prod_object, prod_morphism, prod_compose, prod_identity in *; intros; subst COp; simpl in *; destruct_hypotheses; simpl in *;
+          repeat (apply functional_extensionality_dep; intro); t_with t'
+    ).
+  Defined.
+End HomFunctor.
