1a03ecb8b0a7c18533e4c8740cb40e74585a01b2
Jason Gross <jgross@mit.edu>
2012-06-18 20:29:26 -0400

    Oops.  We want [PathsEquivalent], not [eq].

--- MetaTranslation.v
+++ MetaTranslation.v
@@ -1,6 +1,6 @@
 Require Import Setoid.
 Require Export Translation.
-Require Import Common.
+Require Import Common EquivalenceRelation.
 
 Set Implicit Arguments.
 
@@ -12,7 +12,7 @@ Section MetaTranslation.
   Record MetaTranslation := {
     SComponentsOf :> forall c : C, path D (F c) (G c);
     SCommutes : forall s d (e : C.(Edge) s d),
-      concatenate (F.(PathOf) _ _ e) (SComponentsOf d) = concatenate (SComponentsOf s) (G.(PathOf) _ _ e)
+      PathsEquivalent _ _ _ (concatenate (F.(PathOf) _ _ e) (SComponentsOf d)) (concatenate (SComponentsOf s) (G.(PathOf) _ _ e))
   }.
 End MetaTranslation.
 
@@ -22,33 +22,33 @@ Section MetaTranslationComposition.
   Variable G G' : Translation D E.
 
   Hint Resolve SCommutes f_equal f_equal2.
+  Hint Resolve post_concatenate_equivalent pre_concatenate_equivalent.
 
   (* See NaturalTransformation for documentation *)
   Definition MTComposeMT (T' : MetaTranslation F' F'') (T : MetaTranslation F F') :
     MetaTranslation F F''.
     refine {| SComponentsOf := (fun c => concatenate (T c) (T' c)) |};
-      (* XXX TODO: Find a way to get rid of [m] in the transitivity call *)
-      intros; transitivity (concatenate (concatenate (T _) (PathOf F' _ _ e)) (T' _));
-        solve_repeat_rewrite concatenate_associative eauto.
+      (* XXX TODO: Find a way to get rid of [e] in the transitivity call *)
+      abstract (intros; transitivity (concatenate (concatenate (T _) (PathOf F' _ _ e)) (T' _));
+        solve_repeat_rewrite concatenate_associative eauto).
   Defined.
 
   Hint Rewrite SCommutes.
 
   Ltac strip_concatenate' :=
     match goal with
-      | [ |- concatenate ?x _ = concatenate ?x _ ] => f_equal
-      | [ |- concatenate _ ?x = concatenate _ ?x ] => f_equal
+      | [ |- @RelationsEquivalent _ _ _ _ _ _ (concatenate _ _) (concatenate _ _) ] => apply pre_concatenate_equivalent || apply post_concatenate_equivalent
     end.
   Ltac strip_concatenate :=
     repeat (repeat strip_concatenate';
       repeat (rewrite concatenate_associative; try strip_concatenate'); repeat strip_concatenate';
         repeat (rewrite <- concatenate_associative; try strip_concatenate'); repeat strip_concatenate').
 
-  Hint Extern 1 (_ = _) => strip_concatenate.
+  Hint Extern 1 (@RelationsEquivalent _ _ _ _ _ _ _ _) => strip_concatenate.
 
-  Hint Extern 1 (concatenate (TransferPath _ ?p) _ = concatenate _ (TransferPath _ ?p)) =>
-    induction p; simpl; try apply concatenate_noedges_p;
-      repeat rewrite concatenate_associative; rewrite SCommutes; strip_concatenate; assumption.
+  Hint Extern 1 (@RelationsEquivalent _ _ _ _ _ _ (concatenate (TransferPath _ ?p) _) (concatenate _ (TransferPath _ ?p))) =>
+    induction p; simpl; try rewrite concatenate_noedges_p; try reflexivity;
+      repeat rewrite concatenate_associative; rewrite SCommutes; strip_concatenate; auto.
 
   Definition MTComposeT (U : MetaTranslation G G') (T : MetaTranslation F F'):
     MetaTranslation (ComposeTranslations G F) (ComposeTranslations G' F').
