57f9df32ce1fe2b4b7def3cfa2e64072a6abb749
Jason Gross <jgross@mit.edu>
2012-07-09 23:50:34 -0400

    Define pointed sets.

--- SpecializedSetCategory.v
+++ SpecializedSetCategory.v
@@ -24,3 +24,27 @@ Section CSet.
     |}; abstract (firstorder; etransitivity; eauto; t).
   Defined.
 End CSet.
+
+(* There is a category [Set*], where the objects are sets with a distinguished elements and the morphisms are set morphisms *)
+Section PointedSet.
+  Definition PointedTypeCat : @SpecializedCategory { A : Type & A } (fun s d => (projT1 s) -> (projT1 d)).
+    refine {|
+      Compose' := fun _ _ _ f g => (fun x => f (g x));
+      Identity' := fun _ => (fun x => x)
+    |}; abstract (firstorder; etransitivity; eauto; t).
+  Defined.
+
+  Definition PointedSetCat : @SpecializedCategory { A : Set & A } (fun s d => (projT1 s) -> (projT1 d)).
+    refine {|
+      Compose' := fun _ _ _ f g => (fun x => f (g x));
+      Identity' := fun _ => (fun x => x)
+    |}; abstract (firstorder; etransitivity; eauto; t).
+  Defined.
+
+  Definition PointedPropCat : @SpecializedCategory { A : Prop & A } (fun s d => (projT1 s) -> (projT1 d)).
+    refine {|
+      Compose' := fun _ _ _ f g => (fun x => f (g x));
+      Identity' := fun _ => (fun x => x)
+    |}; abstract (firstorder; etransitivity; eauto; t).
+  Defined.
+End PointedSet.
