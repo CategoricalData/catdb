4017ef643bb13e8fe1e96badfbf300259fee4627
Jason Gross <jgross@mit.edu>
2012-07-10 23:00:48 -0400

    Define coercions between functors to/from [SetCat], [TypeCat], [PropCat].
    Too bad we can't pass around universe variables/sorts.

--- SpecializedSetCategory.v
+++ SpecializedSetCategory.v
@@ -1,6 +1,8 @@
 Require Export SpecializedCategory SpecializedFunctor.
 Require Import Common.
 
+Set Implicit Arguments.
+
 (* There is a category [Set], where the objects are sets and the morphisms are set morphisms *)
 Section CSet.
   Definition TypeCat : @SpecializedCategory Type (fun s d => s -> d).
@@ -24,6 +26,59 @@ Section CSet.
     |}; abstract (firstorder; etransitivity; eauto; t).
   Defined.
 End CSet.
+
+Section SetCoercionsDefinitions.
+  Variable objC : Type.
+  Variable morC : objC -> objC -> Type.
+  Variable C : SpecializedCategory morC.
+
+  Definition SpecializedFunctorToProp := SpecializedFunctor C PropCat.
+  Definition SpecializedFunctorToSet := SpecializedFunctor C SetCat.
+  Definition SpecializedFunctorToType := SpecializedFunctor C TypeCat.
+
+  Definition SpecializedFunctorFromProp := SpecializedFunctor PropCat C.
+  Definition SpecializedFunctorFromSet := SpecializedFunctor SetCat C.
+  Definition SpecializedFunctorFromType := SpecializedFunctor TypeCat C.
+End SetCoercionsDefinitions.
+
+Identity Coercion SpecializedFunctorToProp_Id : SpecializedFunctorToProp >-> SpecializedFunctor.
+Identity Coercion SpecializedFunctorToSet_Id : SpecializedFunctorToSet >-> SpecializedFunctor.
+Identity Coercion SpecializedFunctorToType_Id : SpecializedFunctorToType >-> SpecializedFunctor.
+Identity Coercion SpecializedFunctorFromProp_Id : SpecializedFunctorFromProp >-> SpecializedFunctor.
+Identity Coercion SpecializedFunctorFromSet_Id : SpecializedFunctorFromSet >-> SpecializedFunctor.
+Identity Coercion SpecializedFunctorFromType_Id : SpecializedFunctorFromType >-> SpecializedFunctor.
+
+Section SetCoercions.
+  Variable objC : Type.
+  Variable morC : objC -> objC -> Type.
+  Variable C : SpecializedCategory morC.
+
+  Local Ltac build_functor := hnf in *;
+    match goal with
+      | [ F : SpecializedFunctor _ _ |- SpecializedFunctor ?C ?D ] =>
+        exact (Build_SpecializedFunctor C D
+          F.(ObjectOf')
+          F.(MorphismOf')
+          F.(FCompositionOf')
+          F.(FIdentityOf')
+        )
+    end.
+
+  Definition SpecializedFunctorTo_Prop2Set (F : SpecializedFunctorToProp C) : SpecializedFunctorToSet C. build_functor. Defined.
+  Definition SpecializedFunctorTo_Prop2Type (F : SpecializedFunctorToProp C) : SpecializedFunctorToType C. build_functor. Defined.
+  Definition SpecializedFunctorTo_Set2Type (F : SpecializedFunctorToSet C) : SpecializedFunctorToType C. build_functor. Defined.
+
+  Definition SpecializedFunctorFrom_Set2Prop (F : SpecializedFunctorFromSet C) : SpecializedFunctorFromProp C. build_functor. Defined.
+  Definition SpecializedFunctorFrom_Type2Prop (F : SpecializedFunctorFromType C) : SpecializedFunctorFromProp C. build_functor. Defined.
+  Definition SpecializedFunctorFrom_Type2Set (F : SpecializedFunctorFromType C) : SpecializedFunctorFromSet C. build_functor. Defined.
+End SetCoercions.
+
+Coercion SpecializedFunctorTo_Prop2Set : SpecializedFunctorToProp >-> SpecializedFunctorToSet.
+Coercion SpecializedFunctorTo_Prop2Type : SpecializedFunctorToProp >-> SpecializedFunctorToType.
+Coercion SpecializedFunctorTo_Set2Type : SpecializedFunctorToSet >-> SpecializedFunctorToType.
+Coercion SpecializedFunctorFrom_Set2Prop : SpecializedFunctorFromSet >-> SpecializedFunctorFromProp.
+Coercion SpecializedFunctorFrom_Type2Prop : SpecializedFunctorFromType >-> SpecializedFunctorFromProp.
+Coercion SpecializedFunctorFrom_Type2Set : SpecializedFunctorFromType >-> SpecializedFunctorFromSet.
 
 (* There is a category [Set*], where the objects are sets with a distinguished elements and the morphisms are set morphisms *)
 Section PointedSet.
