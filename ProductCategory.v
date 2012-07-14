d1ff5fd07d3848e4033679c7e674a08659727758
Jason Gross <jgross@mit.edu>
2012-07-14 01:58:56 -0400

    Minor change to ProductCategory

--- ProductCategory.v
+++ ProductCategory.v
@@ -6,7 +6,7 @@ Section ProductCategory.
   Variables C D : Category.
 
   Definition ProductCategory : Category.
-    refine {| Object := ((@Object C) * (@Object D))%type;
+    refine {| Object := (C * D)%type;
       Morphism := (fun s d => ((C.(Morphism) (fst s) (fst d)) * (D.(Morphism) (snd s) (snd d)))%type);
       Identity := (fun o => (Identity (fst o), Identity (snd o)));
       Compose := (fun s d d' m2 m1 => (Compose (fst m2) (fst m1), Compose (snd m2) (snd m1)))
