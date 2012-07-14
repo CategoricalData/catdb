5b618fe38f8ebbe3ad5bcc86b37302de070b59be
Jason Gross <jgross@mit.edu>
2012-07-14 01:37:43 -0400

    Convert some wiki markup to Coq markup.

--- Grothendieck.v
+++ Grothendieck.v
@@ -13,7 +13,7 @@ Section Grothendieck.
      [F : C -> Set]
      be a functor from any small category to the category of sets.
      The Grothendieck construct for [F] is the category [Γ F] whose
-     objects are pairs [(c, x)], where <math>c\in C</math> is an
+     objects are pairs [(c, x)], where [c : C] is an
      object and [x : F c] is an element, and for which the set
      [Hom (Γ F) (c1, x1) (c2, x2)] is the set of morphisms
      [f : c1 -> c2] in [C] such that [F.(MorphismOf) f x1 = x2].
