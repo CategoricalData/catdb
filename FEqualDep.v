ff8dcb3ef9285c5ce07206437ce3cc0ae39126c7
Jason Gross <jgross@mit.edu>
2012-07-25 03:24:58 -0400

    Improve FEqualDep.v
    Among other things, there are now versions of [f_equal2], [f_equal3], etc., for
    [JMeq].  They were generated with the python script:
    
    def f_equaln_JMeq(n):
        rtn = """  Lemma f_equal%d_JMeq """ % n
        for i in range(n):
            rtn += 'A%d ' % i
        rtn += 'B '
        for i in range(n):
            rtn += 'a%d b%d ' % (i, i)
        rtn += '(f : forall'
        for i in range(n):
            rtn += ' (a%d : A%d' % (i, i)
            for j in range(i):
                rtn += ' a%d' % j
            rtn += ')'
        rtn += ', B'
        for i in range(n):
            rtn += ' a%d' % i
        rtn += ') :\n    a0 = b0\n'
        for i in range(1, n):
            rtn += '    -> (a0 = b0'
            for j in range(1, i + 1):
                rtn += ' -> a%d == b%d' % (j, j)
            rtn += ')\n'
        rtn += '    -> f'
        for i in range(n):
            rtn += ' a%d' % i
        rtn += ' == f'
        for i in range(n):
            rtn += ' b%d' % i
        rtn += """.
        intros.
        repeat (firstorder; repeat subst).
      Qed.
    """
        return rtn
    
    
    for i in range(1, 20):
        print(f_equaln_JMeq(i))

--- FEqualDep.v
+++ FEqualDep.v
@@ -6,13 +6,93 @@ Set Implicit Arguments.
 Local Infix "==" := JMeq (at level 70).
 
 Section f_equal_dep.
-  Lemma f_equal_JMeq A A' B B' a b (f : forall a : A, A' a) (g : forall b : B, B' b) :
-    f == g -> (f == g -> A == B) -> (f == g -> A == B -> A' == B') -> (f == g -> A == B -> a == b) -> f a == g b.
+  Theorem f_type_equal {A B A' B'} : A = A' -> B = B' -> (A -> B) = (A' -> B').
+    intros; repeat subst; reflexivity.
+  Qed.
+
+  Theorem forall_extensionality_dep : forall {A}
+    (f g : A -> Type),
+    (forall x, f x = g x) -> (forall x, f x) = (forall x, g x).
+    intros.
+    replace f with g; auto.
+    apply functional_extensionality_dep; auto.
+  Qed.
+
+  Theorem forall_extensionality_dep_JMeq : forall {A B}
+    (f : A -> Type) (g : B -> Type),
+    A = B -> (A = B -> forall x y, x == y -> f x == g y) -> (forall x, f x) = (forall x, g x).
+    intros; firstorder; intuition; repeat subst.
+    apply forall_extensionality_dep.
+    intro.
+    apply JMeq_eq.
+    eauto.
+  Qed.
+
+
+  Lemma JMeq_eqT A B (x : A) (y : B) : x == y -> A = B.
+    intro H; destruct H; reflexivity.
+  Qed.
+
+  Lemma fg_equal_JMeq A B B' (f : forall a : A, B a) (g : forall a : A, B' a) x :
+    f == g -> (f == g -> B = B') -> f x == g x.
     intros.
     repeat (firstorder; repeat subst).
   Qed.
 
-  Lemma eq_implies_JMeq T (A B : T) : A = B -> A == B.
+  Lemma f_equal_JMeq A A' B B' a b (f : forall a : A, A' a) (g : forall b : B, B' b) :
+    f == g -> (f == g -> A' == B') -> (f == g -> a == b) -> f a == g b.
+    intros.
+    firstorder.
+    repeat destruct_type @JMeq.
+    repeat subst; reflexivity.
+  Qed.
+
+  Lemma f_equal1_JMeq A0 B a0 b0 (f : forall (a0 : A0), B a0) :
+    a0 = b0
+    -> f a0 == f b0.
+    intros.
+    repeat (firstorder; repeat subst).
+  Qed.
+
+  Lemma f_equal2_JMeq A0 A1 B a0 b0 a1 b1 (f : forall (a0 : A0) (a1 : A1 a0), B a0 a1) :
+    a0 = b0
+    -> (a0 = b0 -> a1 == b1)
+    -> f a0 a1 == f b0 b1.
+    intros.
+    repeat (firstorder; repeat subst).
+  Qed.
+
+  Lemma f_equal3_JMeq A0 A1 A2 B a0 b0 a1 b1 a2 b2 (f : forall (a0 : A0) (a1 : A1 a0) (a2 : A2 a0 a1), B a0 a1 a2) :
+    a0 = b0
+    -> (a0 = b0 -> a1 == b1)
+    -> (a0 = b0 -> a1 == b1 -> a2 == b2)
+    -> f a0 a1 a2 == f b0 b1 b2.
+    intros.
+    repeat (firstorder; repeat subst).
+  Qed.
+
+  Lemma f_equal4_JMeq A0 A1 A2 A3 B a0 b0 a1 b1 a2 b2 a3 b3 (f : forall (a0 : A0) (a1 : A1 a0) (a2 : A2 a0 a1) (a3 : A3 a0 a1 a2), B a0 a1 a2 a3) :
+    a0 = b0
+    -> (a0 = b0 -> a1 == b1)
+    -> (a0 = b0 -> a1 == b1 -> a2 == b2)
+    -> (a0 = b0 -> a1 == b1 -> a2 == b2 -> a3 == b3)
+    -> f a0 a1 a2 a3 == f b0 b1 b2 b3.
+    intros.
+    repeat (firstorder; repeat subst).
+  Qed.
+
+  Lemma f_equal5_JMeq A0 A1 A2 A3 A4 B a0 b0 a1 b1 a2 b2 a3 b3 a4 b4 (f : forall (a0 : A0) (a1 : A1 a0) (a2 : A2 a0 a1) (a3 : A3 a0 a1 a2) (a4 : A4 a0 a1 a2 a3), B a0 a1 a2 a3 a4) :
+    a0 = b0
+    -> (a0 = b0 -> a1 == b1)
+    -> (a0 = b0 -> a1 == b1 -> a2 == b2)
+    -> (a0 = b0 -> a1 == b1 -> a2 == b2 -> a3 == b3)
+    -> (a0 = b0 -> a1 == b1 -> a2 == b2 -> a3 == b3 -> a4 == b4)
+    -> f a0 a1 a2 a3 a4 == f b0 b1 b2 b3 b4.
+    intros.
+    repeat (firstorder; repeat subst).
+  Qed.
+
+  Lemma eq_JMeq T (A B : T) : A = B -> A == B.
     intro; subst; reflexivity.
   Qed.
 
@@ -26,18 +106,21 @@ Section f_equal_dep.
     subst; reflexivity.
   Qed.
 
-  Theorem forall_extensionality_dep : forall {A}
-    (f g : A -> Type),
-    (forall x, f x = g x) -> (forall x, f x) = (forall x, g x).
+  Theorem functional_extensionality_dep_JMeq' : forall {A1 A2} {B1 : A1 -> Type} {B2 : A2 -> Type},
+    forall (f : forall x : A1, B1 x) (g : forall x : A2, B2 x),
+      A1 = A2
+      -> (A1 = A2 -> forall x y, x == y -> B1 x = B2 y)
+      -> (A1 = A2 -> forall x y, x == y -> f x == g y) -> f == g.
     intros.
-    replace f with g; auto.
-    apply functional_extensionality_dep; auto.
+    intuition; repeat subst.
+    apply functional_extensionality_dep_JMeq; intros;
+      intuition.
   Qed.
 End f_equal_dep.
 
 Ltac JMeq_eq :=
   repeat match goal with
-           | [ |- _ == _ ] => apply eq_implies_JMeq
+           | [ |- _ == _ ] => apply eq_JMeq
            | [ H : _ == _ |- _ ] => apply JMeq_eq in H
          end.
 
@@ -59,3 +142,15 @@ Ltac f_equal_dep := intros; f_equal; simpl in *;
            | [ |- ?f ?a == ?g ?b ] => f_equal_dep'
            | _ => try reflexivity
          end; f_equal_dep_cleanup.
+
+Section misc.
+  Lemma sig_JMeq A0 A1 B0 B1 (a : @sig A0 A1) (b : @sig B0 B1) : A1 == B1 -> proj1_sig a == proj1_sig b -> a == b.
+    intros.
+    destruct_sig.
+    repeat destruct_type @JMeq.
+    JMeq_eq; subst.
+    JMeq_eq.
+    simpl_eq.
+    trivial.
+  Qed.
+End misc.
