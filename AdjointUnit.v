a5225a03b3c7d905c15c705ecaec1915a1cf2c5d
Jason Gross <jgross@mit.edu>
2013-03-05 18:05:41 -0500

    Add a version of adjoint with both unit and counit
    Still need to prove it's equivalent to the other versions...

--- AdjointUnit.v
+++ AdjointUnit.v
@@ -50,7 +50,7 @@ Section Adjunction.
     categories ``there exists an adjunction,'' but rather ``this functor has an adjoint''
     that we are concerned with.
     **)
-  Definition AdjunctionUnit (F : Functor C D) (G : Functor D C) :=
+  Definition AdjunctionUnit :=
     { T : NaturalTransformation (IdentityFunctor C) (ComposeFunctors G F) &
       forall (c : C) (d : D) (f : C.(Morphism) c (G d)),
         { g : D.(Morphism) (F c) d | unique (fun g => Compose (G.(MorphismOf) g) (T c) = f) g }
@@ -89,9 +89,83 @@ Section Adjunction.
     Terminology and notation:
     * The statement (o) is the UMP of the counit [U].
     **)
-  Definition AdjunctionCounit (F : Functor C D) (G : Functor D C) :=
+  Definition AdjunctionCounit :=
     { U : NaturalTransformation (ComposeFunctors F G) (IdentityFunctor D) &
       forall (c : C) (d : D) (g : D.(Morphism) (F c) d),
         { f : C.(Morphism) c (G d) | unique (fun f => Compose (U d) (F.(MorphismOf) f) = g) f }
+    }.
+
+  (** Quoting Wikipedia on Adjoint Functors:
+
+      A counit-unit adjunction between two categories [C] and [D]
+      consists of two functors [F : C ← D] and [G : C → D and two
+      natural transformations
+
+      [[
+      ε : FG → 1_C
+      η : 1_D → GF
+      ]]
+
+      respectively called the counit and the unit of the adjunction
+      (terminology from universal algebra), such that the compositions
+
+      [[
+          F η            ε F
+      F -------> F G F -------> F
+
+          η G            G ε
+      G -------> G F G -------> G
+      ]]
+
+      are the identity transformations [1_F] and [1_G] on [F] and [G]
+      respectively.
+
+      In this situation we say that ``[F] is left adjoint to [G]'' and
+      ''[G] is right adjoint to [F]'', and may indicate this
+      relationship by writing [(ε, η) : F ⊣ G], or simply [F ⊣ G].
+
+      In equation form, the above conditions on (ε, η) are the
+      counit-unit equations
+
+      [[
+      1_F = ε F o F η
+      1_G = G ε o η G
+      ]]
+
+      which mean that for each [X] in [C] and each [Y] in [D],
+
+      [[
+      1_{FY} = ε_{FY} o F(η_Y)
+      1_{GX} = G(ε_X) o η_{GX}
+      ]]
+
+      These equations are useful in reducing proofs about adjoint
+      functors to algebraic manipulations.  They are sometimes called
+      the ``zig-zag equations'' because of the appearance of the
+      corresponding string diagrams.  A way to remember them is to
+      first write down the nonsensical equation [1 = ε o η] and then
+      fill in either [F] or [G] in one of the two simple ways which
+      make the compositions defined.
+
+      Note: The use of the prefix ``co'' in counit here is not
+      consistent with the terminology of limits and colimits, because
+      a colimit satisfies an initial property whereas the counit
+      morphisms will satisfy terminal properties, and dually.  The
+      term unit here is borrowed from the theory of monads where it
+      looks like the insertion of the identity 1 into a monoid.
+   *)
+
+  Local Reserved Notation "'ε'".
+  Local Reserved Notation "'η'".
+
+  (* Use the per-object version of the equations, so that we don't need the associator in the middle *)
+  Record AdjunctionUnitCounit :=
+    {
+      Adjunction_Unit : NaturalTransformation (IdentityFunctor C) (ComposeFunctors G F)
+                                              where "'η'" := Adjunction_Unit;
+      Adjunction_Counit : NaturalTransformation (ComposeFunctors F G) (IdentityFunctor D)
+                                                where "'ε'" := Adjunction_Counit;
+      Adjunction_UnitCounitEquation1 : forall Y : C, Compose (ε (F Y)) (F.(MorphismOf) (η Y)) = Identity (F Y);
+      Adjunction_UnitCounitEquation2 : forall X : D, Compose (G.(MorphismOf) (ε X)) (η (G X)) = Identity (G X)
     }.
 End Adjunction.
