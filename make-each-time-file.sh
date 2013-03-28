6fae7a46ed8effad7c28975e2390c56b3cf6c90a
Jason Gross <jgross@mit.edu>
2013-03-28 01:19:55 -0400

    add some clean-up code

--- make-each-time-file.sh
+++ make-each-time-file.sh
@@ -19,6 +19,9 @@ if [ -z "$(cat $SHELF_NAME)" ]; then
     # there is no diff, so just copy the time file
     cp "$OLD_FILE" "$NEW_FILE"
 else
+    if [ -z "$(hg diff)" ]; then
+	hg revert -a # clean up any, e.g., chmod +x
+    fi
     hg import --no-commit $SHELF_NAME && mv $SHELF_NAME "$SHELF_NAME-$(date | base64).bak"
     make clean
     make timed 2>&1 | tee "$NEW_FILE"
