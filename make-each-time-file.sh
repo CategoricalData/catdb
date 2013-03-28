d0de798b0fcb95a18910bf13e413dae77577cdbd
Jason Gross <jgross@mit.edu>
2013-03-27 21:47:44 -0400

    Fix bash script to not need hg shelve

--- make-each-time-file.sh
+++ make-each-time-file.sh
@@ -5,15 +5,16 @@ OLD_FILE="$2"
 
 SHELF_NAME="compare-times-shelf"
 
-trap "hg unshelve --name $SHELF_NAME" SIGINT SIGTERM
+trap "hg import --no-commit $SHELF_NAME" SIGINT SIGTERM
 
 # make the old version
-hg shelve --all --name $SHELF_NAME
+#hg shelve --all --name $SHELF_NAME
+hg diff > $SHELF_NAME && hg revert -a
 make clean
 make timed 2>&1 | tee "$OLD_FILE"
 
 
 # make the current version
-hg unshelve --all --name $SHELF_NAME
+hg import --no-commit $SHELF_NAME && mv $SHELF_NAME "$SHELF_NAME-$(date | base64).bak"
 make clean
 make timed 2>&1 | tee "$NEW_FILE"
