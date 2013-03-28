40ec6081c3117725eff3afee3579839def5bf4c3
Jason Gross <jgross@mit.edu>
2013-03-28 02:39:13 -0400

    Add printout of the total time spent compiling.

--- make-both-time-files.py
+++ make-both-time-files.py
@@ -28,6 +28,17 @@ def get_sorted_file_list_from_times_dict(times_dict, descending=True):
         return (int(minutes), float(seconds))
     return sorted(times_dict.keys(), key=get_key, reverse=descending)
 
+def sum_times(times):
+    def to_seconds(time):
+        minutes, seconds = time.replace('s', '').split('m')
+        return int(minutes) * 60 + float(seconds)
+    seconds = sum(map(to_seconds, times))
+    minutes = int(seconds) / 60
+    seconds -= minutes * 60
+    full_seconds = int(seconds)
+    partial_seconds = int(1000 * (seconds - full_seconds))
+    return '%dm%02d.%03ds' % (minutes, full_seconds, partial_seconds)
+
 def make_table_string(left_times_dict, right_times_dict,
                       descending=True,
                       left_tag="After", right_tag="Before"):
@@ -37,11 +48,15 @@ def make_table_string(left_times_dict, right_times_dict,
     middle_width = max(map(len, names + ["File Name"]))
     format_string = "%%-%ds | %%-%ds | %%-%ds" % (left_width, middle_width, right_width)
     header = format_string % (left_tag, "File Name", right_tag)
+    footer = format_string % (sum_times(left_times_dict.values()),
+                              "Total",
+                              sum_times(right_times_dict.values()))
     sep = '-' * len(header)
     return '\n'.join([header, sep] + [format_string % (left_times_dict[name],
                                                        name,
                                                        right_times_dict[name])
-                                      for name in names])
+                                      for name in names] +
+                     [sep, footer])
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
