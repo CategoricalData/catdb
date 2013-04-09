cc3563550f9dc38be83e3f49adec0d2ee03f7ff9
Jason Gross <jgross@mit.edu>
2013-04-09 16:14:44 -0400

    Add UNION and UNION ALL

--- dbs.v
+++ dbs.v
@@ -1,4 +1,4 @@
-Require Import List.
+Require Import List Setoid Classes.RelationClasses.
 
 Set Implicit Arguments.
 
@@ -41,15 +41,20 @@ Inductive Database : DatabaseType -> Type :=
 
 Definition RowHead T Ts (r : Row (T :: Ts)) : T :=
   match r with
-    | RNil => tt
     | RCons _ _ v _ => v
   end.
 
 Definition RowTail T Ts (r : Row (T :: Ts)) : Row Ts :=
   match r with
-    | RNil => tt
     | RCons _ _ _ r' => r'
   end.
+
+Lemma RowEta T Ts r : RCons (@RowHead T Ts r) (@RowTail T Ts r) = r.
+  refine match r with
+           | RCons _ _ v r' => _
+         end.
+  reflexivity.
+Qed.
 
 Fixpoint getColumn T R (c : Column T R) : Row R -> T :=
   match c with
@@ -59,13 +64,11 @@ Fixpoint getColumn T R (c : Column T R) : Row R -> T :=
 
 Definition DatabaseHead R Rs (d : Database (R :: Rs)) : Table R :=
   match d with
-    | DNil => tt
     | DCons _ _ r _ => r
   end.
 
 Definition DatabaseTail R Rs (d : Database (R :: Rs)) : Database Rs :=
   match d with
-    | DNil => tt
     | DCons _ _ _ d' => d'
   end.
 
@@ -82,6 +85,9 @@ Inductive ColumnList : RowType -> RowType -> Type :=
 | CNil : ColumnList nil nil
 | CCons : forall T fromTs toTs, Column T fromTs -> ColumnList fromTs toTs -> ColumnList fromTs (T :: toTs).
 
+(** [SelectFromTable fromR toR cs] is the SQL query [SELECT cs FROM
+    fromR], and [toR] is the resulting schema.  This operation is
+    specific to a single table. *)
 Fixpoint SelectFromTable fromR toR (cs : ColumnList fromR toR) : Row fromR -> Row toR
   := match cs in (ColumnList fromR toR) return (Row fromR -> Row toR) with
        | CNil =>
@@ -89,3 +95,193 @@ Fixpoint SelectFromTable fromR toR (cs : ColumnList fromR toR) : Row fromR -> Ro
        | CCons T fromTs toTs c cs' =>
          fun r => RCons (getColumn c r) (SelectFromTable cs' r)
      end.
+
+(** [UnionAllTables2 r t1 t2] is the SQL query [SELECT *
+    FROM t1 UNION ALL SELECT * FROM t2]. *)
+Definition UnionAllTables2 (r : RowType) (t1 t2 : Table r) : Table r
+  := t1 ++ t2.
+
+(** [UnionAllTables r [t1, t2, ..., tn]] is the SQL query [SELECT *
+    FROM t1 UNION ALL SELECT * FROM t2 UNION ALL ... UNION ALL SELECT
+    * FROM tn]. *)
+Definition UnionAllTables (r : RowType) (ts : list (Table r)) : Table r
+  := match ts with
+       | nil => nil
+       | t :: ts => fold_left (@UnionAllTables2 r) ts t
+     end.
+
+Lemma RNil_unique : forall x : Row nil, x = RNil.
+  intros; refine (match x in Row Ts return match Ts return Row Ts -> Prop with
+                                             | nil => fun x => x = RNil
+                                             | _ => fun _ => True
+                                           end x with
+                    | RNil => eq_refl
+                    | _ => I
+                  end).
+Qed.
+
+Lemma RowNil_unique : forall x y : Row nil, x = y.
+  intros x y.
+  rewrite (RNil_unique x).
+  rewrite (RNil_unique y).
+  reflexivity.
+Qed.
+
+Local Hint Immediate RowNil_unique.
+
+(** Equality of rows is decidable if equality of all constituent types
+    is decidable. *)
+Inductive RowTypeDecidable (P : forall T, relation T) `(forall T, Equivalence (P T))
+: RowType -> Type :=
+| RTDecNil : RowTypeDecidable P _ nil
+| RTDecCons : forall T Ts, (forall t0 t1 : T,
+                              {P T t0 t1} + {~P T t0 t1})
+                           -> RowTypeDecidable P _ Ts
+                           -> RowTypeDecidable P _ (T :: Ts).
+
+Definition RowTypeDecHead P H T Ts (r : RowTypeDecidable P H (T :: Ts))
+: forall t0 t1 : T, {P T t0 t1} + {~ P T t0 t1} :=
+  match r with
+    | RTDecCons _ _ v _ => v
+  end.
+
+Definition RowTypeDecTail P H T Ts (r : RowTypeDecidable P H (T :: Ts))
+: RowTypeDecidable P H Ts :=
+  match r with
+    | RTDecCons _ _ _ r' => r'
+  end.
+
+Lemma RowTypeDecidableEta P H T Ts (r : RowTypeDecidable P H (T :: Ts))
+: RTDecCons (@RowTypeDecHead P H T Ts r) (@RowTypeDecTail P H T Ts r) = r.
+  refine match r with
+           | RTDecCons _ _ v r' => _
+         end.
+  reflexivity.
+Qed.
+
+Lemma Cons_not_eq
+: forall T Ts (h1 : T) (t1 : Row Ts) h2 t2,
+    (h1 <> h2 \/ t1 <> t2)
+    -> RCons h1 t1 <> RCons h2 t2.
+  intuition;
+  match goal with
+    | [ H : RCons _ _ = RCons _ _ |- _ ] => (apply (f_equal (@RowHead _ _)) in H; tauto)
+                                              || (apply (f_equal (@RowTail _ _)) in H; tauto)
+  end.
+Qed.
+
+Hint Resolve Cons_not_eq.
+
+Fixpoint Row_eq Ts
+: RowTypeDecidable (@eq) _ Ts -> forall r1 r2 : Row Ts, {r1 = r2} + {r1 <> r2}.
+Proof.
+  refine match Ts return RowTypeDecidable _ _ Ts -> forall r1 r2 : Row Ts, {r1 = r2} + {r1 <> r2} with
+           | nil => fun _ _ _ => left _
+           | _ :: Ts' => fun D r1 r2 =>
+                           if (RowTypeDecHead D) (RowHead r1) (RowHead r2)
+                           then if Row_eq _ (RowTypeDecTail D) (RowTail r1) (RowTail r2)
+                                then left _
+                                else right _
+                           else right _
+         end;
+  clear Row_eq;
+  abstract (repeat match goal with
+                     | [ r : Row nil |- _ ] => (generalize (RNil_unique r); intro; subst)
+                     | [ r : Row (_ :: _) |- _ ] => progress (rewrite <- (RowEta r) in *; simpl in *; idtac)
+                   end; auto; congruence).
+Defined.
+
+(** If a type has decidable equality, so does [In] *)
+Fixpoint In_dec A (eq_dec : forall x y : A, {x = y} + {x <> y}) (l : list A)
+: forall x, {In x l} + {~In x l}
+  := match l as l return forall x, {In x l} + {~In x l} with
+       | nil
+         => fun _ => right (fun x : False => match x with end)
+       | b :: m
+         => fun x
+            => match eq_dec b x with
+                 | left H
+                   => left (or_introl H)
+                 | right not_eq
+                   => match In_dec eq_dec m x with
+                        | left H
+                          => left (or_intror H)
+                        | right not_in
+                          => right (fun H'
+                                    => match H' return False with
+                                         | or_introl H''
+                                           => not_eq H''
+                                         | or_intror H''
+                                           => not_in H''
+                                       end)
+                      end
+               end
+     end.
+
+(** Takes in a list, and returns the list of unique elements in that list *)
+Fixpoint unique_from A (eq_dec : forall x y : A, {x = y} + {x <> y})
+         (l : list A) : list A :=
+  match l with
+    | nil => nil
+    | b :: t => if In_dec eq_dec t b
+                then unique_from eq_dec t
+                else b :: unique_from eq_dec t
+  end.
+Arguments unique_from _ _ !l / .
+
+Inductive list_duplicate_free A : list A -> Prop :=
+| nil_duplicate_free : list_duplicate_free nil
+| cons_duplicate_free : forall x l, list_duplicate_free l -> ~In x l -> list_duplicate_free (x :: l).
+
+Lemma unique_no_change_in A (l : list A) eq_dec
+: forall x, In x l <-> In x (@unique_from  A eq_dec l).
+  intro x; split; intro;
+  induction l;
+  repeat match goal with
+           | _ => easy
+           | _ => solve [ intuition ]
+           | _ => solve [ exfalso; intuition ]
+           | [ H : _ \/ _ |- _ ] => destruct H
+           | _ => progress (simpl in *; subst)
+           | [ H : appcontext[match ?E with _ => _ end] |- _ ] => destruct E
+           | [ |- appcontext[match ?E with _ => _ end] ] => destruct E
+         end.
+Qed.
+
+Lemma unique_from_no_duplicates A (l : list A) eq_dec
+: list_duplicate_free l -> @unique_from A eq_dec l = l.
+  intro H.
+  induction H; [ reflexivity | ].
+  simpl.
+  match goal with
+    | [ |- appcontext[match ?E with _ => _ end] ] => destruct E
+  end;
+    try solve [ exfalso; abstract intuition ].
+  f_equal.
+  assumption.
+Qed.
+
+Lemma no_duplicates_unique_from A (l : list A) eq_dec
+: list_duplicate_free (@unique_from A eq_dec l).
+  induction l; [ simpl; constructor | ].
+  repeat match goal with
+           | _ => progress simpl in *
+           | _ => assumption
+           | [ |- appcontext[match ?E with _ => _ end] ] => destruct E
+           | _ => intro
+           | _ => progress intuition
+           | _ => constructor
+           | [ H : _ |- _ ] => apply <- unique_no_change_in in H
+         end.
+Qed.
+
+(** [UnionTables2 r t1 t2] is the SQL query [SELECT *
+    FROM t1 UNION SELECT * FROM t2]; it removes duplicates *)
+Definition UnionTables2 (r : RowType) (H : RowTypeDecidable _ _ r) (t1 t2 : Table r) : Table r
+  := unique_from (Row_eq H) (UnionAllTables2 t1 t2).
+
+(** [UnionTables r [t1, t2, ..., tn]] is the SQL query [SELECT * FROM
+    t1 UNION SELECT * FROM t2 UNION ... UNION SELECT * FROM tn].  It
+    removes duplicates. *)
+Definition UnionTables (r : RowType) (H : RowTypeDecidable _ _ r) (ts : list (Table r)) : Table r
+  := unique_from (Row_eq H) (UnionAllTables ts).
